6b8971763cb29849974bce5554d51f28c871f5c8
swatantra94 <swatantranigam9451@gmail.com>
2020-07-26 00:29:13 +0530

    First Commit for portfolio

---
+++ main/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from main import views
+
+
+urlpatterns = [
+    path('',views.index,name='index'),
+    path('languages/',views.languages,name='languages'),
+    path('projects/',views.projects,name = 'projects'),
+ ]
